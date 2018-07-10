25d5a5fde5a311370f1b1cc9087875a96872aec7
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-10 10:40:10 +0100

    Create config file for chat kit + introduce lifecycle methods in the App component for the chat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import { ChatManager, TokenProvider } from '@pusher/chatkit'
 import Chatkit from '@pusher/chatkit'
 import MessageList from './Components/MessageList'
 // import SendMessageForm from './Components/SendMessageForm'
@@ -6,7 +7,32 @@ import MessageList from './Components/MessageList'
 // import NewRoomForm from './Components/NewRoomForm'
 import './App.css';
 
+import { tokenUrl, instanceLocator } from './config'
+
 class App extends Component {
+
+  componentDidMount() {
+      const chatManager = new Chatkit.ChatManager({
+          instanceLocator,
+          userId: 'giada',
+          tokenProvider: new Chatkit.TokenProvider({
+              url: tokenUrl
+          })
+      })
+
+      chatManager.connect()
+      .then(currentUser => {
+          currentUser.subscribeToRoom({
+              roomId: 11325949,
+              hooks: {
+                  onNewMessage: message => {
+                      console.log('message.text: ', message.text);
+                  }
+              }
+          })
+      })
+  }
+
   render() {
     return (
       <div className="app">
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl =
+"https://us1.pusherplatform.io/services/chatkit_token_provider/v1/4ecc91b0-6fb8-4f98-bbf4-7c5991b90f7f/token";
+const instanceLocator = "v1:us1:4ecc91b0-6fb8-4f98-bbf4-7c5991b90f7f";
+
+export { tokenUrl, instanceLocator }
