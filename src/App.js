98bbcbc9f3c31560692db8dc3e146408deff5fbe
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-10 09:29:28 +0100

    Create a MessageList component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MessageList from './Components/MessageList'
+// import SendMessageForm from './Components/SendMessageForm'
+// import RoomList from './Components/RoomList'
+// import NewRoomForm from './Components/NewRoomForm'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+
+      <MessageList />
+
       </div>
     );
   }
